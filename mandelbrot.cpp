74c7cfeeff5a45f22611c2f3664bfbea2cc81893
Dhruv-Patil7 <dhruvmpatil2007@gmail.com>
2026-06-23 23:34:12 +0530

    Add initial implementation of Mandelbrot set with multithreading support

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -7,64 +7,76 @@
 using namespace std;
 
 class MandelbrotSet{
-public:
+  public:
     vector<int> image;
     //Setting the size of graph
     double REAL_MIN = -2;
     double REAL_MAX = 2;
-    double IMAGE_MAX = 1.5;
     double IMAGE_MIN = -1.5;
+    double IMAGE_MAX = 1.5;
 
     int HEIGHT;
     int WIDTH;
     const int MAX_ITR;
 
-    MandelbrotSet(int height, int width, int max_itr) : 
-                  WIDTH(width), HEIGHT(height), MAX_ITR(max_itr){
+    MandelbrotSet(int height, int width, int max_itr, int numThreads) : 
+                  WIDTH(width), HEIGHT(height), MAX_ITR(max_itr), NUM_THREADS(numThreads){
         image.resize(WIDTH * HEIGHT);// Making the vector the size of total pixels.
     }
+    
+    //renders fractal using multiplethreading
+    void renderFract(){
+        vector<thread> threads;
+        NUM_THREADS = thread::hardware_concurrency();
+        if(NUM_THREADS == 0) NUM_THREADS = 4; // Fallback
 
-    void renderFract() {
-        for(int y = 0; y < HEIGHT; y++){
+        int rowsPerThread = HEIGHT / NUM_THREADS;
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            int startRow = i * rowsPerThread;
+            int endRow = (i == NUM_THREADS - 1) ? HEIGHT : startRow + rowsPerThread;
+            threads.push_back(thread(&MandelbrotSet::renderFractPart, this, startRow, endRow));
+        }
+
+        for (auto& t : threads) {
+            t.join();
+        }
+    }
+
+    //Changing the region (Zooming In);
+    void updateRegion(double realMin, double realMax, double imageMin, double imageMax){
+        REAL_MIN = realMin;
+        REAL_MAX = realMax;
+        IMAGE_MIN = imageMin;
+        IMAGE_MAX = imageMax;
+    }
+    
+    //Setting it to default values.
+    void defaultRegion(){
+        REAL_MIN = -2;
+        REAL_MAX = 2;
+        IMAGE_MIN = -1.5;
+        IMAGE_MAX = 1.5;
+    }
+  private:
+    int NUM_THREADS;//Count of threads.
+
+    void renderFractPart(int startrow, int endrow) {
+        for(int y = startrow; y < endrow; y++){
             for(int x = 0; x < WIDTH; x++){
-                //Calculating the value of c.
-                complex<double> c((REAL_MIN + (REAL_MAX - REAL_MIN) * x / WIDTH),
-                                IMAGE_MIN + (IMAGE_MAX - IMAGE_MIN) * y / HEIGHT);
+                double realP = REAL_MIN + (REAL_MAX - REAL_MIN) * x / WIDTH;
+                double imagP = IMAGE_MIN + (IMAGE_MAX - IMAGE_MIN) * y / HEIGHT;
+                complex<double> c(realP, imagP);//Calculating the value of C.
                 complex<double> z = 0;
                 int itr = 0;
-                //Calculating the number of iterations.
+                //Calculating the number of iteration for each pixels.
                 while(norm(z) <= 4.0 && itr < MAX_ITR){
                     z = z * z + c;
                     itr++;
                 }
-                //Inserting the number of iteration to each pixels.
+                //Storing the number of iteration.
                 image[y * WIDTH + x] = itr;
             }
         }
     }
-
-    void saveFract(const string &fileloc){
-        ofstream ofs(fileloc, ios::binary);//Opening the current branch.
-        ofs << "P5\n" << WIDTH << " " << HEIGHT << "\n255\n";
-
-        for(int i = 0; i < HEIGHT * WIDTH; i++){
-            int itr = image[i];
-            unsigned char color = static_cast<unsigned char>(255 * itr / MAX_ITR);
-            ofs << color;//Assigning the color according to the number of iterations.
-        }
-        ofs.close();//Closing the branch.
-    }
 };
-
-int main(){
-    int height, width, max_itr;
-    cout << "Enter <height, width, max_itr>" << endl;
-    cin >> height >> width >> max_itr;
-
-    MandelbrotSet fractal = MandelbrotSet(height, width, max_itr);
-    fractal.renderFract();
-    cout << "Fractal Rendered" << endl; 
-
-    fractal.saveFract("mandelbrot.pgm");
-    cout << "executed" << endl;
-}
